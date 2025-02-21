c978471b8173051171e6a429ad9e2b68f3a619f5
Ron-J <ronandrew.j2023@vitstudent.ac.in>
2025-02-21 13:16:43 +0530

    Add files via upload

---
+++ Assignment-10/Q2.js
@@ -0,0 +1,2 @@
+document.getElementById("red").onclick= ()=>document.body.style="background-color: red;";
+document.getElementById("blue").onclick= ()=>document.body.style="background-color: blue;";
